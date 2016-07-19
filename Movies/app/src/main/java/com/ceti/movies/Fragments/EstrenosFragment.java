804d9941482bbe866e302a6f0a6734dcdf77151b
Angel Sirlopu C <design.anguel@gmail.com>
2016-07-18 21:57:31 -0500

    Fragmentos

---
+++ Movies/app/src/main/java/com/ceti/movies/Fragments/EstrenosFragment.java
@@ -0,0 +1,16 @@
+package com.ceti.movies.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class EstrenosFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
--- Movies/app/src/main/java/com/ceti/movies/MainActivity.java
+++ Movies/app/src/main/java/com/ceti/movies/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ceti.movies.Fragments.EstrenosFragment;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,12 +24,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         viewPager = (ViewPager) findViewById(R.id.viewPager);
-        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
-
+        //viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+        setupFragments(viewPager);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
     }
-
+    public void setupFragments(ViewPager viewPager){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new EstrenosFragment(),"Estrenos");
+        viewPager.setAdapter(viewPagerAdapter);
+    }
     class ViewPagerAdapter extends FragmentPagerAdapter {
         private List<Fragment> listFragment = new ArrayList<>();
         private List<String> titles = new ArrayList<>();
@@ -41,6 +47,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public Fragment getItem(int position) {
             return listFragment.get(position);
+        }
+        public void addFragment(Fragment fragment, String title){
+            listFragment.add(fragment);
+            titles.add(title);
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
         }
     }
 }
