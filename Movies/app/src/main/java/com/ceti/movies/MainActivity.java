8a817f38c8714751955575bf440db84bebe6fcff
Angel Sirlopu C <design.anguel@gmail.com>
2016-07-18 21:43:09 -0500

    Creación de la clase ViewPagerAdapter

--- Movies/app/src/main/java/com/ceti/movies/MainActivity.java
+++ Movies/app/src/main/java/com/ceti/movies/MainActivity.java
@@ -1,13 +1,46 @@
 package com.ceti.movies;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private List<Fragment> listFragment = new ArrayList<>();
+        private List<String> titles = new ArrayList<>();
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public int getCount() {
+            return listFragment.size();
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return listFragment.get(position);
+        }
     }
 }
